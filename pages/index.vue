386e4a95a7836fd3917aa3c68074cd44c17086ee
MinJun Choi <mj043000@naver.com>
2022-07-08 03:36:54 +0900

    soon landing page will be updated

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,18 @@
         <div class="pt-16 pb-10 md:pt-24  md:pb-10 max-w-7xl mx-auto text-6xl md:text-9xl text-white text-center">
             <span class=" pt-8 font-extrabold tracking-tight ">ZeroPage</span>
         </div>
-
-        <div class="flex space-x-2 md:space-x-4 justify-center text-xs md:text-xl">
+        <Search class="max-w-3xl"/>
+        <div class="flex space-x-2 md:space-x-4 pb-10 md:pb-20 md:pt-6 justify-center text-xs md:text-xl">
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#중앙대학교</div>
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#소프트웨어학부</div>
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#학술연구회</div>
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#since1991</div>
         </div>
         
-    </div>
+        
 
-    
+        
+    </div>    
 </template>
 
 <script>
