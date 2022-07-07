972c262db1b0c71cb8f59857ad12771fdd2aafa7
MinJun Choi <mj043000@naver.com>
2022-07-08 00:51:13 +0900

    change font detail

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 
 <template>
     <div class="w-full z-0 bg-zinc-900 section h-content" >
-        <div class="pt-16 pb-10 md:pt-24  md:pb-10 max-w-7xl mx-auto text-6xl md:text-9xl text-white text-center group-hover:shadow-lg transform group-hover:scale-105 transition group-hover:duration-500">
+        <div class="pt-16 pb-10 md:pt-24  md:pb-10 max-w-7xl mx-auto text-6xl md:text-9xl text-white text-center">
             <span class=" pt-8 font-extrabold tracking-tight ">ZeroPage</span>
         </div>
 
@@ -9,6 +9,7 @@
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#중앙대학교</div>
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#소프트웨어학부</div>
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#학술연구회</div>
+                <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#since1991</div>
         </div>
         
     </div>
