7f89eef6a8b98c080d12a0e655ad54c3e3334547
MinJun Choi <mj043000@naver.com>
2022-09-03 11:54:15 +0900

    Fix: mobile layout changed

--- pages/sprouthon.vue
+++ pages/sprouthon.vue
@@ -11,33 +11,51 @@
         </div>
 
         <div class="max-w-7xl grid grid-cols-1 colspan mt-5 md:mt-8 pb-14 md:pb-24 mx-auto px-6">
-            <div class="group">
-                <div class=" flex items-center  py-5 md:py-8 ">
-                  <div class="sbox">
-                    <img  class="profile" :src="require(`~/assets/resources/sprouthon/01.jpg`)" alt="">
-                  </div>
-                  <div class=" px-0 md:px-4 md:pl-9 ">
-                      <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-green-400">TEAM-자바보시든지</p>
-                      <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-green-300 group-hover:duration-500" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
-                      <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
-                  </div>
+            <div class="group hidden md:block">
+              <div class=" flex items-center  py-5 md:py-8 ">
+                <div class="sbox">
+                  <img  class="profile" :src="require(`~/assets/resources/sprouthon/01.jpg`)" alt="">
                 </div>
+                <div class=" px-0 md:px-4 md:pl-9 ">
+                  <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-green-400">TEAM-자바보시든지</p>
+                  <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-green-300 group-hover:duration-500" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
+                  <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
+                </div>
+              </div>
             </div>
 
-            <div class="group">
-                <div class=" flex items-center  py-5 md:py-8 justify-end">
-                  <div class=" px-0 md:px-4 md:pr-9 text-right">
-                      <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-green-400">TEAM-자바보시든지</p>
-                      <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-green-300 group-hover:duration-500" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
-                      <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
-                  </div>
-                  <div class="sbox">
-                    <img  class="profile" :src="require(`~/assets/resources/sprouthon/01.jpg`)" alt="">
-                  </div>
+            <div class="group hidden md:block">
+              <div class=" flex items-center  py-5 md:py-8 justify-end">
+                <div class=" px-0 md:px-4 md:pr-9 text-right">
+                  <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-green-400">TEAM-자바보시든지</p>
+                  <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-green-300 group-hover:duration-500" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
+                  <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
                 </div>
+                <div class="sbox">
+                  <img  class="profile" :src="require(`~/assets/resources/sprouthon/01.jpg`)" alt="">
+                </div>
+              </div>
             </div>
-        </div>
-        
+
+            
+
+            <div class="md:hidden">
+              <img :src="require(`~/assets/resources/sprouthon/01.jpg`)" class="rounded-t-xl border-none rounded-b-xl"/>
+            </div>
+            <div class="md:hidden text-center px-4 py-6">
+              <a class="font-general-semibold text-xl text-white font-semibold mb-2 hover:text-green-400" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
+              <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
+            </div>
+
+            <div class="md:hidden mt-4">
+              <img :src="require(`~/assets/resources/sprouthon/01.jpg`)" class="rounded-t-xl border-none rounded-b-xl"/>
+            </div>
+            <div class="md:hidden text-center px-4 py-6">
+              <a class="font-general-semibold text-xl text-white font-semibold mb-2 hover:text-green-400" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
+              <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
+            </div>
+
+      </div>
     </div>
 </template>
 
