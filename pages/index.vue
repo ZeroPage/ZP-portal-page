1a6dca07e483146c950c08617f4d8c6378cc1c8a
MinJun Choi <mj043000@naver.com>
2022-09-04 23:23:51 +0900

    add sprouthon image

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,9 @@
         <div class="pt-16 pb-10 md:pt-24  md:pb-10 max-w-7xl mx-auto text-6xl md:text-9xl text-white text-center">
             <span class=" pt-8 font-extrabold tracking-tight ">ZeroPage</span>
         </div>
+
         <Search class="max-w-3xl"/>
+
         <div class="flex space-x-2 md:space-x-4 pb-10 md:pb-20 md:pt-6 justify-center text-xs md:text-xl">
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#중앙대학교</div>
                 <div type="" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">#소프트웨어학부</div>
@@ -29,10 +31,11 @@
 </template>
 
 <script>
-import aosMixin from '~/mixins/aos'
+import aosMixin from '~/mixins/aos';
+
 export default {
   name: 'PageIndex',
-  mixins: [aosMixin]
+  mixins: [aosMixin],
 }
 </script>
 
--- pages/sprouthon.vue
+++ pages/sprouthon.vue
@@ -27,12 +27,12 @@
             <div class="group hidden md:block">
               <div class=" flex items-center  py-5 md:py-8 justify-end">
                 <div class=" px-0 md:px-4 md:pr-9 text-right">
-                  <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-green-400">TEAM-자바보시든지</p>
-                  <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-green-300 group-hover:duration-500" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
-                  <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
+                  <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-blue-400">TEAM-Champion</p>
+                  <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-blue-300 group-hover:duration-500" href="https://github.com/Junyoungeun/GrowingBud" target="blank">새싹 키우기 (GrowingBud)</a>
+                  <p class=" text-sm md:text-base text-white custom-text">전영은</p>
                 </div>
                 <div class="sbox">
-                  <img  class="profile" :src="require(`~/assets/resources/sprouthon/01.jpg`)" alt="">
+                  <img  class="profile" :src="require(`~/assets/resources/sprouthon/03.jpg`)" alt="">
                 </div>
               </div>
             </div>
@@ -48,11 +48,11 @@
             </div>
 
             <div class="md:hidden mt-4">
-              <img :src="require(`~/assets/resources/sprouthon/01.jpg`)" class="rounded-t-xl border-none rounded-b-xl"/>
+              <img :src="require(`~/assets/resources/sprouthon/03.jpg`)" class="rounded-t-xl border-none rounded-b-xl"/>
             </div>
             <div class="md:hidden text-center px-4 py-6">
-              <a class="font-general-semibold text-xl text-white font-semibold mb-2 hover:text-green-400" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
-              <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
+              <a class="font-general-semibold text-xl text-white font-semibold mb-2 hover:text-blue-400" href="https://github.com/Junyoungeun/GrowingBud" target="blank">카페 키오스크 개발</a>
+              <p class=" text-sm md:text-base text-white custom-text">전영은</p>
             </div>
 
       </div>
