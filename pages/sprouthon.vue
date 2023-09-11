1784282e709fa541df3fac5d03a804a3a70aabc4
polarpolar914 <kdw2003914@naver.com>
2023-09-11 21:47:30 +0900

    UPDATE 2023

--- pages/sprouthon.vue
+++ pages/sprouthon.vue
@@ -17,9 +17,9 @@
                   <img  class="profile" :src="require(`~/assets/resources/sprouthon/01.jpg`)" alt="">
                 </div>
                 <div class=" px-0 md:px-4 md:pl-9 ">
-                  <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-green-400">TEAM-자바보시든지</p>
-                  <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-green-300 group-hover:duration-500" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
-                  <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
+                  <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-green-400">TEAM-01</p>
+                  <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-green-300 group-hover:duration-500" href="https://github.com/beomjunback/sprouthon" target="blank">범준 가챠</a>
+                  <p class=" text-sm md:text-base text-white custom-text">백범준</p>
                 </div>
               </div>
             </div>
@@ -27,9 +27,9 @@
             <div class="group hidden md:block">
               <div class=" flex items-center  py-5 md:py-8 justify-end">
                 <div class=" px-0 md:px-4 md:pr-9 text-right">
-                  <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-blue-400">TEAM-Champion</p>
-                  <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-blue-300 group-hover:duration-500" href="https://github.com/Junyoungeun/GrowingBud" target="blank">새싹 키우기 (GrowingBud)</a>
-                  <p class=" text-sm md:text-base text-white custom-text">전영은</p>
+                  <p class="mb-1 md:mb-1.5 text-sm md:text-sm text-blue-400">TEAM-02</p>
+                  <a class="mb-1 md:mb-1.5 text-lg md:text-2xl font-semibold  text-white transition group-hover:text-blue-300 group-hover:duration-500" href="https://github.com/uki0903/Bricks-Breaker-in-C" target="blank">벽돌깨기</a>
+                  <p class=" text-sm md:text-base text-white custom-text">최형욱 · 박지원</p>
                 </div>
                 <div class="sbox">
                   <img  class="profile" :src="require(`~/assets/resources/sprouthon/02.jpg`)" alt="">
@@ -37,23 +37,23 @@
               </div>
             </div>
 
-            
+
 
             <div class="md:hidden">
               <img :src="require(`~/assets/resources/sprouthon/01.jpg`)" class="rounded-t-xl border-none rounded-b-xl"/>
             </div>
             <div class="md:hidden text-center px-4 py-6">
-              <a class="font-general-semibold text-xl text-white font-semibold mb-2 hover:text-green-400" href="https://github.com/hyun-hyang/SPROUTHON-YouCanGrabJAVA" target="blank">카페 키오스크 개발</a>
-              <p class=" text-sm md:text-base text-white custom-text">임지민 · 박창훈</p>
+              <a class="font-general-semibold text-xl text-white font-semibold mb-2 hover:text-green-400" href="https://github.com/beomjunback/sprouthon" target="blank">범준 가챠</a>
+              <p class=" text-sm md:text-base text-white custom-text">백범준</p>
             </div>
 
             <div class="md:hidden mt-4">
               <img :src="require(`~/assets/resources/sprouthon/02.jpg`)" class="rounded-t-xl border-none rounded-b-xl"/>
-              
+
             </div>
             <div class="md:hidden text-center px-4 py-6">
-              <a class="font-general-semibold text-xl text-white font-semibold mb-2 hover:text-blue-400" href="https://github.com/Junyoungeun/GrowingBud" target="blank">새싹 키우기 (GrowingBud)</a>
-              <p class=" text-sm md:text-base text-white custom-text">전영은</p>
+              <a class="font-general-semibold text-xl text-white font-semibold mb-2 hover:text-blue-400" href="https://github.com/uki0903/Bricks-Breaker-in-C" target="blank">벽돌깨기</a>
+              <p class=" text-sm md:text-base text-white custom-text">최형욱 · 박지원</p>
             </div>
 
       </div>
