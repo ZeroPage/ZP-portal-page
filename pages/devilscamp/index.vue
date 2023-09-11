771f7126008e91f39163918d8f08fb2cda15f2b0
polarpolar914 <kdw2003914@naver.com>
2023-09-11 23:13:35 +0900

    UPDATE SPROUTHON

--- pages/devilscamp/index.vue
+++ pages/devilscamp/index.vue
@@ -6,7 +6,7 @@
                 Devils Camp
             </div>
             <div class="font-normal text-base md:text-xl text-slate-400 text-center">
-                2022 ZP의 데블스캠프 기록입니다.
+                2023 ZP의 데블스캠프 기록입니다.
             </div>
             <div class="font-normal text-base md:text-xl text-blue-300 text-right hover:text-blue-500 hover:underline">
                 <nuxt-link :to="{path: '/pre-devilscamp'}" replace>올해의 데블스캠프를 알아보아요!</nuxt-link>
@@ -16,7 +16,7 @@
         <div data-aos="fade-up" class="max-w-7xl flex grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 mx-auto px-6 sm:gap-7 pb-18 md:pb-8">
             <div  class="rounded-xl border-2 border-zinc-900 hover:border-white cursor-pointer mb-10 sm:mb-0 px-4 py-4 group" v-for="devilscamp of devilscamps" :key="devilscamp">
                 <nuxt-link :to='`devilscamp/${devilscamp.slug}`'>
-                    
+
                     <div class="">
                         <img class="rounded-t-xl border-none rounded-b-xl" :src="require(`~/assets/resources/devilscamp/${devilscamp.img}`)" alt="">
                     </div>
@@ -24,9 +24,9 @@
                         <h2 class="mb-1 md:mb-1.5 text-lg md:text-xl font-semibold  text-white transition group-hover:text-blue-400 group-hover:duration-500">{{ devilscamp.title }}</h2>
                         <p class=" text-sm md:text-base text-white custom-text">{{devilscamp.speaker}}</p>
                     </div>
-                    
+
                 </nuxt-link>
-            </div>           
+            </div>
         </div>
     </div>
 </template>
