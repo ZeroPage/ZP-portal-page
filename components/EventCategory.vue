3da110129fb580966154a51f08532ad4b0e3754d
MinJun Choi <mj043000@naver.com>
2023-10-06 12:46:46 +0900

    Feat: category added

--- components/EventCategory.vue
+++ components/EventCategory.vue
@@ -31,6 +31,26 @@
                     </div> -->
                 </nuxt-link>
             </div>
+            <div class="rounded-xl border-2 border-zinc-900 hover:border-white cursor-pointer mb-10 sm:mb-0 px-2 py-2">
+                <nuxt-link to="/pre-angelscamp">
+                    <div>
+                        <img :src="require(`~/assets/resources/category/angelscamp.jpg`)" class="rounded-t-xl border-none rounded-b-xl"/>
+                    </div>
+                    <!-- <div class="text-center px-4 py-6">
+                        <p class="font-general-semibold text-xl text-white font-semibold mb-2">Devils Camp</p>
+                    </div> -->
+                </nuxt-link>
+            </div>
+            <div class="rounded-xl border-2 border-zinc-900 hover:border-white cursor-pointer mb-10 sm:mb-0 px-2 py-2">
+                <nuxt-link to="/pre-jigeumgeuddae">
+                    <div>
+                        <img :src="require(`~/assets/resources/category/jigeumgeuddae.jpg`)" class="rounded-t-xl border-none rounded-b-xl"/>
+                    </div>
+                    <!-- <div class="text-center px-4 py-6">
+                        <p class="font-general-semibold text-xl text-white font-semibold mb-2">Devils Camp</p>
+                    </div> -->
+                </nuxt-link>
+            </div>
              
         </div>
     </div>
