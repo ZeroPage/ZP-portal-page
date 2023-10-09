02109ccb3927c6a79f1af3fa1def31fa3f03bb23
MinJun Choi <mj043000@naver.com>
2023-10-09 18:46:39 +0900

    Style: delete dark mode

--- pages/index.vue
+++ pages/index.vue
@@ -91,19 +91,19 @@
         <div data-aos="fade-up" class=" grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
             <div>
                 <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-white">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         동아리 가입은 어떻게 하나요?
                     </h3>
-                    <p class="text-gray-500 dark:text-gray-400"><a class="underline" href="https://zeropage.org/">메인 사이트</a>
+                    <p class="text-gray-500"><a class="underline" href="https://zeropage.org/">메인 사이트</a>
                     에 접속하셔서 회원가입 후 인사게시판에 '준회원 신청합니다'라는 제목으로 양식에 맞춰 글을 올리시면 회장단이 1-3일 내에 확인하고, 카카오톡 채팅방에 초대해드리고 있습니다.</p>
                 </div>
                 <div class="mb-10">                        
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-white">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         프로그래밍 경험이 없어도 동아리 내에서 활동할 수 있나요?
                     </h3>
-                    <p class="text-gray-500 dark:text-gray-400">스터디마다 스터디원들에게 요구하는 최소 수준이 있긴 하지만, 다양한 스터디가 존재하니 괜찮습니다. 처음부터 잘하는 사람은 없으니까요.
+                    <p class="text-gray-500">스터디마다 스터디원들에게 요구하는 최소 수준이 있긴 하지만, 다양한 스터디가 존재하니 괜찮습니다. 처음부터 잘하는 사람은 없으니까요.
                     혹은 원하는 스터디나, 본인에게 맞는 스터디가 '없다!'면 축하드립니다. 그 분야에 대해서 가장 잘 아는 사람이군요! 직접 스터디를 만들어보시는 건 어떨까요?</p>
                 </div>
                 <div class="mb-10">
@@ -111,34 +111,34 @@
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         신입생이 아닌 고학년이어도 가입가능한가요?
                     </h3>
-                    <p class="text-gray-500 dark:text-gray-400">학년에 무관하게 프로그래밍에 관심이 있는 누구든 모두 제로페이지에 가입가능합니다!</p>
+                    <p class="text-gray-500 ">학년에 무관하게 프로그래밍에 관심이 있는 누구든 모두 제로페이지에 가입가능합니다!</p>
                 </div>
                 
             </div>
             <div>
               <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-white">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         동아리에서는 무엇을 하나요?
                     </h3>
-                    <p class="text-gray-500 dark:text-gray-400">제로페이지에서는 매주 수요일마다 정기 모임을 가집니다. 정기 모임에서는 동아리 주요 행사들(데블스캠프, 새싹교실 등)에 대한 공지, 진행상황 보고를 하고 OMS도 같이 진행합니다.
+                    <p class="text-gray-500 ">제로페이지에서는 매주 수요일마다 정기 모임을 가집니다. 정기 모임에서는 동아리 주요 행사들(데블스캠프, 새싹교실 등)에 대한 공지, 진행상황 보고를 하고 OMS도 같이 진행합니다.
                     OMS는 제로페이지 회원들이 본인의 관심사에 대해 작은 세미나를 여는 것으로 흥미로운 주제들에 대해 10-30분 정도 발표하는 것입니다.
                     <a class="underline" href="https://wiki.zeropage.org/wiki.php">제로페이지 위키</a>에 'oms'라고 검색하시면 자세한 설명이 나오니 한번 읽어보세요!</p>
                 </div>
               
                 <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-white">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         모든 행사에 필수적으로 참여해야하나요?
                     </h3>
-                    <p class="text-gray-500 dark:text-gray-400">아닙니다! 본인의 스케줄에 맞게 원하는 행사만 골라서 참여하실 수 있습니다!</p>
+                    <p class="text-gray-500 ">아닙니다! 본인의 스케줄에 맞게 원하는 행사만 골라서 참여하실 수 있습니다!</p>
                 </div>
                 <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-white">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         동아리 회비가 있나요?
                     </h3>
-                    <p class="text-gray-500 dark:text-gray-400">없습니다! 감사하게도 졸업하신 선배님들이 '지피지기'라는 이름으로 필요할 때마다 자금을 지원해주신 덕에 무리 없이 유익한 행사들을 진행할 수 있습니다.</p>
+                    <p class="text-gray-500 ">없습니다! 감사하게도 졸업하신 선배님들이 '지피지기'라는 이름으로 필요할 때마다 자금을 지원해주신 덕에 무리 없이 유익한 행사들을 진행할 수 있습니다.</p>
                 </div>
                 
             </div>
