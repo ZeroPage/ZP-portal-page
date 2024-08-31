e356adc423ba051e612af10c226638c3e418f89e
MinJun Choi <mj043000@naver.com>
2024-09-01 00:31:57 +0900

    fix: oms 폴더명 변경

--- pages/oms/[oms].vue
+++ pages/oms/[oms].vue
@@ -4,35 +4,34 @@
 
         <div class="pt-20 md:pt-15 pb-3 md:pb-10 max-w-7xl mx-auto px-6">
             <div class="pb-6 text-3xl md:text-5xl font-semibold text-white ">
-                {{oms.title}} 
+                {{data.title}} 
             </div>
             <div class="font-normal text-base md:text-xl text-white pb-12">
-                by {{oms.speaker}}
+                by {{data.speaker}}
             </div>
 
             <div class="py-10 grid justify-items-stretch">
-                <embed :src="`/pdf/${oms.pdf}`" height="750px"/>
+                <embed :src="`/oms/${data.pdf}`" height="750px"/>
             </div>
         </div>
-
-        
     </div>
-
 </template>
 
-<script setup>
-const { path } = useRoute();
-const { data } = await useAsyncData(`content-${path}`, async () => {
-  // fetch document where the document path matches with the current route
-  let oms = queryContent().where({ _path: path }).findOne();
-  
+<script setup lang="ts">
 
+const { path } = useRoute()
+const { data: articles } = await useAsyncData(`oms-${path}`, () => queryContent(path).findOne())
+
+
+
+const data = computed(() => {
   return {
-    oms: await oms,
-    
-  };
-});
-
+    title: articles.value?.title || 'no-title available',
+    date: articles.value?.date || 'no-date-available',
+    speaker: articles.value?.speaker || 'no-speaker-available',
+    pdf: articles.value?.pdf || 'no-pdf-available',
+  }
+})
 </script>
 
 <style scoped>
